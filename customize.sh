72a6515eb4b5ccd8572aafb15e7b871cac20f6ad
igithublab <66892031+igithublab@users.noreply.github.com>
2020-08-02 08:20:34 +0800

    Add more customize command
    Hostname
    LAN IP address
    NTP settings
    WIFI SSID

--- customize.sh
+++ customize.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-#=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: eSirPlayground
-# Youtube Channel: https://goo.gl/fvkdwm 
-#=================================================
-#1. Modify default IP
-sed -i 's/192.168.1.1/192.168.11.21/g' openwrt/package/base-files/files/bin/config_generate
-
-
-#!/bin/bash
 # Modify default Hostname
 #sed -i "s/hostname='OpenWrt'/hostname='YourHostname'/g" package/base-files/files/bin/config_generate
 
